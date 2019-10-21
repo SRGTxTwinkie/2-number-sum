0d1722b29acc6825c7dfe8f6391da35255c6cc59
SRGTxTwinkie <zanedreisbig@yahoo.com>
2019-10-21 17:32:48 -0400

    Done

---
+++ NumberSum.rb
@@ -0,0 +1,15 @@
+def Numbersum
+
+  print "First Number: "
+  num1 = gets.to_i
+
+  print "Seconds Num: "
+  num2 = gets.to_i
+
+  print "#{num1} + #{num2} = #{num1 + num2} \n"
+
+end
+
+loop do
+  Numbersum()
+end
